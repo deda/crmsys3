5d92f67b4a01a18ae3e76b7501b86834b0f085a2
Alex Karagichev <karagichev@mtote.net>
2011-10-27 21:11:16 +0400

    1. добавил локали 2. продолжаю подготовку к запуску

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,12 @@
 # Filters added to this controller apply to all controllers in the application.
 # Likewise, all the methods added will be available for all controllers.
+require 'search'
 
 class ApplicationController < ActionController::Base
   include Clearance::Authentication
   include Search
   helper :all # include all helpers, all the time
   protect_from_forgery # See ActionController::RequestForgeryProtection for details
-
-  # Scrub sensitive parameters from your log
-  filter_parameter_logging :password
 
   helper_method :signed_in_as_admin?
   helper_method :signed_in_as_account_admin?
